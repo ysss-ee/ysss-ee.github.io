fc7480e148e6caf5bc91f6c3a538463785159783
ysss-ee <xnmym_2020@qq.com>
2026-08-16 03:20:01 +0800

    Deploy new blog

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = 'Yuan\'s Blog';
 export const GITHUB_URL = 'https://github.com/ysss-ee';
 export const SITE_DESCRIPTION = '欢迎来到我的世界';
+export const PERSONAL_DESCRIPTION = '大二在读';
